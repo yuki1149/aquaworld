dd49b027049e0f2ee8645dc054ede86f2936964d
yuki1149 <work.yuki.abe@gmail.com>
2024-09-17 23:59:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,35 @@ $(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+
+    $(".humberger").on("click", function () {
+        if ($("header").hasClass("open")) {
+            $("header").removeClass("open");
+        } else {
+            $("header").addClass("open");
+        }
+    });
+
+    $(".menu").on("click", function () {
+        $("header").removeClass("open");
+    });
+
+    $('a[href^="#"]').click(function () {
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? "html" : href);
+        let position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, 600, "swing");
+        return false;
     });
 });
